f2dbf0f5904b1303fef58719a8aa05172709cbdb
EmreDegirmenci <emre.degirmenci@textalkmedia.se>
2022-10-10 17:47:08 +0300

    Adds AppDelegate, AppRouter and SceneDelegat

--- Sisal/App/AppDelegate.swift
+++ Sisal/App/AppDelegate.swift
@@ -10,11 +10,17 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+  var window: UIWindow?
+  var restrictRotation:UIInterfaceOrientationMask = .portrait
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
     return true
+  }
+
+  func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask
+  {
+    return self.restrictRotation
   }
 
   // MARK: UISceneSession Lifecycle
